2ac916ea01348bb88dbdec8e9d6ed5ceee4e6eaf
SuperElectronBrain <qkrdlfwn75@gmail.com>
2023-08-30 21:05:30 +0900

    2023-08-30 change1
    시계 UI 작업 완료

---
+++ Assets/Scripts/UI/ButtonScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+	// Start is called before the first frame update
+	protected virtual void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+	//protected virtual void Update()
+	//{
+	//    
+	//}
+
+	public virtual void OnButtonClick()
+	{
+
+	}
+}
