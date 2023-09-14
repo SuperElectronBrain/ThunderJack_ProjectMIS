59a22da3873ab54ad9d515b3a0528e0771728d5e
hiver3489 <hiver3489@naver.com>
2023-09-14 16:02:00 +0900

    CameraEffect

---
+++ Assets/Scripts/Camera/CameraEffectZone.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraEffectZone : MonoBehaviour
+{
+    [SerializeField]
+    CinemachineVirtualCamera vCam;
+    [SerializeField]
+    CinemachineTrackedDolly dolly;
+
+    Transform playerPos;
+
+    void Start()
+    {
+        dolly = vCam.GetCinemachineComponent<CinemachineTrackedDolly>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            vCam.Priority = 100;
+            playerPos = other.transform;
+            StartCoroutine(StartCameraMove());
+            //dolly.m_AutoDolly.m_Enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StopCoroutine(StartCameraMove());
+            vCam.Priority = 0;
+            playerPos = null;            
+            //dolly.m_AutoDolly.m_Enabled = false;
+        }
+    }
+
+    IEnumerator StartCameraMove()
+    {
+        while(true)
+        {
+            if (playerPos == null)
+                yield break;
+            dolly.m_PathPosition = dolly.m_Path.FindClosestPoint(playerPos.position, 0, -1, 10);
+            yield return null;
+        }
+    }
+}
