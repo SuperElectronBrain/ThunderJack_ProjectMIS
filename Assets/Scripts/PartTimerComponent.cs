6a51fc0b10ee6545c66f191ef2a04878e9b076c8
SuperElectronBrain <qkrdlfwn75@gmail.com>
2023-09-16 18:47:29 +0900

    2023-09-16 change2
    씬 연결 작업 완료

--- Assets/Scripts/PartTimerComponent.cs
+++ Assets/Scripts/PartTimerComponent.cs
@@ -1,137 +1,137 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
+//using System.Collections;
+//using System.Collections.Generic;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class PartTimerComponent : MonoBehaviour
-{
-	[HideInInspector] private CharacterBase master;
-	private Inventory masterInventory;
-	public ItemCode productionItem = ItemCode.RubyRing;
-	[SerializeField] private RecipeBook recipeBook;
-	[HideInInspector] public TextMeshProUGUI m_TMP;
+//public class PartTimerComponent : MonoBehaviour
+//{
+//	[HideInInspector] private CharacterBase master;
+//	private Inventory masterInventory;
+//	public ItemCode productionItem = ItemCode.RubyRing;
+//	[SerializeField] private RecipeBook recipeBook;
+//	[HideInInspector] public TextMeshProUGUI m_TMP;
 
-	[SerializeField] private float productionTime = 5.0f;
-	private float timeLeftForCompletion = 0.0f;
-	private bool workState = false;
+//	[SerializeField] private float productionTime = 5.0f;
+//	private float timeLeftForCompletion = 0.0f;
+//	private bool workState = false;
 
-	// Start is called before the first frame update
-	void Start()
-	{
+//	// Start is called before the first frame update
+//	void Start()
+//	{
 		
-	}
+//	}
 
-	// Update is called once per frame
-	void Update()
-	{
-		float DeltaTime = Time.deltaTime;
+//	// Update is called once per frame
+//	void Update()
+//	{
+//		float DeltaTime = Time.deltaTime;
 
-		if (workState == true)
-		{
-			timeLeftForCompletion = timeLeftForCompletion + DeltaTime;
+//		if (workState == true)
+//		{
+//			timeLeftForCompletion = timeLeftForCompletion + DeltaTime;
 
-			if(m_TMP != null)
-			{
-				m_TMP.text = (int)(productionTime - timeLeftForCompletion) + "";
-			}
+//			if(m_TMP != null)
+//			{
+//				m_TMP.text = (int)(productionTime - timeLeftForCompletion) + "";
+//			}
 
-			if (timeLeftForCompletion > productionTime)
-			{
-				CraftItem();
-				timeLeftForCompletion = 0;
-			}
-		}
-	}
+//			if (timeLeftForCompletion > productionTime)
+//			{
+//				CraftItem();
+//				timeLeftForCompletion = 0;
+//			}
+//		}
+//	}
 
-	public void SetMaster(CharacterBase p_Master)
-	{
-		master = p_Master;
-		if (p_Master != null)
-		{
-			masterInventory = p_Master.gameObject.GetComponent<Inventory>();
-		}
-		else if(p_Master == null)
-		{
-			masterInventory = null;
-		}
-	}
+//	public void SetMaster(CharacterBase p_Master)
+//	{
+//		master = p_Master;
+//		if (p_Master != null)
+//		{
+//			masterInventory = p_Master.gameObject.GetComponent<Inventory>();
+//		}
+//		else if(p_Master == null)
+//		{
+//			masterInventory = null;
+//		}
+//	}
 
-	private void CraftItem()
-	{
-		if (master != null)
-		{
-			if (masterInventory != null)
-			{
-				if(recipeBook != null)
-				{
-					Debug.Log("î");
+//	private void CraftItem()
+//	{
+//		if (master != null)
+//		{
+//			if (masterInventory != null)
+//			{
+//				if(recipeBook != null)
+//				{
+//					Debug.Log("î");
 
-					ItemRecipe itemRecipe = null;
-					List<ItemRecipe> recipes = new List<ItemRecipe>(recipeBook.Recipes);
-					for(int i = 0; i < recipes.Count; i = i + 1)
-					{
-						if (recipes[i].result.itemCode == productionItem)
-						{
-							itemRecipe = recipes[i];
-							break;
-						}
-					}
+//					ItemRecipe itemRecipe = null;
+//					List<ItemRecipe> recipes = new List<ItemRecipe>(recipeBook.Recipes);
+//					for(int i = 0; i < recipes.Count; i = i + 1)
+//					{
+//						if (recipes[i].result.itemCode == productionItem)
+//						{
+//							itemRecipe = recipes[i];
+//							break;
+//						}
+//					}
 
-					if(itemRecipe != null)
-					{
-						bool isCraftable = true;
-						for (int i = 0; i < itemRecipe.ingredients.Count; i = i + 1)
-						{
-							if (masterInventory.FindItem(itemRecipe.ingredients[i].itemCode, itemRecipe.ingredients[i].itemAmount) == false)
-							{
-								isCraftable = false;
-								break;
-							}
-						}
+//					if(itemRecipe != null)
+//					{
+//						bool isCraftable = true;
+//						for (int i = 0; i < itemRecipe.ingredients.Count; i = i + 1)
+//						{
+//							if (masterInventory.FindItem(itemRecipe.ingredients[i].itemCode, itemRecipe.ingredients[i].itemAmount) == false)
+//							{
+//								isCraftable = false;
+//								break;
+//							}
+//						}
 
-						if(isCraftable == true)
-						{
-							for (int i = 0; i < itemRecipe.ingredients.Count; i = i + 1)
-							{
-								masterInventory.PopItem(itemRecipe.ingredients[i].itemCode, itemRecipe.ingredients[i].itemAmount);
-							}
+//						if(isCraftable == true)
+//						{
+//							for (int i = 0; i < itemRecipe.ingredients.Count; i = i + 1)
+//							{
+//								masterInventory.PopItem(itemRecipe.ingredients[i].itemCode, itemRecipe.ingredients[i].itemAmount);
+//							}
 
-							masterInventory.AddItem(itemRecipe.result);
-						}
-					}
-				}
-			}
-		}
-	}
+//							masterInventory.AddItem(itemRecipe.result);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
 
-	public void StartProduction()
-	{
-		if (master != null)
-		{
-			if (masterInventory != null)
-			{
-				if (workState == false)
-				{
-					workState = true;
-					timeLeftForCompletion = 0;
-				}
-			}
-		}
-	}
+//	public void StartProduction()
+//	{
+//		if (master != null)
+//		{
+//			if (masterInventory != null)
+//			{
+//				if (workState == false)
+//				{
+//					workState = true;
+//					timeLeftForCompletion = 0;
+//				}
+//			}
+//		}
+//	}
 
-	public void EndProduction()
-	{
-		if (master != null)
-		{
-			if (masterInventory != null)
-			{
-				if (workState == true)
-				{
-					workState = false;
-					timeLeftForCompletion = 0;
-				}
-			}
-		}
-	}
-}
+//	public void EndProduction()
+//	{
+//		if (master != null)
+//		{
+//			if (masterInventory != null)
+//			{
+//				if (workState == true)
+//				{
+//					workState = false;
+//					timeLeftForCompletion = 0;
+//				}
+//			}
+//		}
+//	}
+//}
--- Assets/Scripts/UI/GameStartButtonScript.cs
+++ Assets/Scripts/UI/GameStartButtonScript.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameStartButtonScript : ButtonScript
 {
+	[SerializeField] private Vector3 destination;
 	[SerializeField] private string destinationSceneName;
+	private PlayerCharacter m_PlayerCharacter;
 
 	// Start is called before the first frame update
 	protected override void Start()
@@ -19,11 +22,59 @@ public class GameStartButtonScript : ButtonScript
 	//
 	//}
 
+	private void OnSceneLoaded(UnityEngine.SceneManagement.Scene p_Scene, UnityEngine.SceneManagement.LoadSceneMode p_Mode)
+	{
+		PlayerCharacter t_PlayerCharacter = null;
+		List<PlayerCharacter> t_PlayerCharacters = FindObjectsOfType<PlayerCharacter>().ToList();
+		for (int i = 0; i < t_PlayerCharacters.Count; i = i + 1)
+		{
+			if (t_PlayerCharacters[i] != m_PlayerCharacter)
+			{
+				t_PlayerCharacter = t_PlayerCharacters[i];
+				break;
+			}
+		}
+		if (t_PlayerCharacter == null)
+		{
+			t_PlayerCharacter = Instantiate(m_PlayerCharacter.gameObject).GetComponent<PlayerCharacter>();
+		}
+
+		if (t_PlayerCharacter != null)
+		{
+			t_PlayerCharacter.gameObject.transform.position = destination;
+			if(m_PlayerCharacter != null)
+			{ 
+				if(t_PlayerCharacter.m_Inventory == null)
+				{
+					t_PlayerCharacter.m_Inventory = t_PlayerCharacter.GetComponent<Inventory>();
+				}
+				if(t_PlayerCharacter.m_Inventory != null)
+				{
+					t_PlayerCharacter.m_Inventory.TransferInventoryItems(m_PlayerCharacter.m_Inventory); 
+				}
+			}
+			t_PlayerCharacter.FindPlayerCharacterUIScript();
+		}
+
+		if(m_PlayerCharacter != null) { Destroy(m_PlayerCharacter.gameObject); }
+		m_PlayerCharacter = null;
+		UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+		Destroy(gameObject);
+	}
+
 	public override void OnButtonClick()
 	{
 		base.OnButtonClick();
 		if (UnityEngine.SceneManagement.SceneManager.GetSceneByName(destinationSceneName) != null)
 		{
+			m_PlayerCharacter = FindObjectOfType<PlayerCharacter>();
+			if (m_PlayerCharacter != null)
+			{
+				DontDestroyOnLoad(m_PlayerCharacter.gameObject);
+				DontDestroyOnLoad(gameObject);
+				UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+			}
+
 			UnityEngine.SceneManagement.SceneManager.LoadScene(destinationSceneName);
 		}
 	}
