904e678d995e18b8d4db35f6692b5e5debdbd7ac
hiver3489 <hiver3489@naver.com>
2023-10-07 02:47:10 +0900

    BuildScene Camera Move

--- Assets/5. Scripts/Camera/InvisibleObjRender.cs
+++ Assets/5. Scripts/Camera/InvisibleObjRender.cs
@@ -23,12 +23,28 @@ public class InvisibleObjRender : MonoBehaviour
         //invisibleObjs = Physics.OverlapBox()
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("InvisibleObj"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("InvisibleObj"))
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
     private void OnDrawGizmos()
     {
         if (playerTransform == null)
             return;
         Vector3 centerPos = transform.position + (playerTransform.position - transform.position) / 2;
-        float boxSize = (playerTransform.position - transform.position).sqrMagnitude;
+        float boxSize = Vector3.Distance(playerTransform.position, transform.position);
 
         Debug.Log(boxSize);
 
