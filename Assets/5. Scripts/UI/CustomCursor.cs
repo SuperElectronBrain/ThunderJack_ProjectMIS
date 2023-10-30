f1c7f88d145337173c378b2f3d60c74bb3dcc36e
SuperElectronBrain <qkrdlfwn75@gmail.com>
2023-10-30 23:31:05 +0900

    2023-10-30 change13
    마우스 커서 리소스 적용

---
+++ Assets/5. Scripts/UI/CustomCursor.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomCursor : MonoBehaviour
+{
+    private RectTransform rectTransform;
+	[SerializeField] private RectTransform Idle;
+	[SerializeField] private RectTransform Click;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+       // base.Start();
+		rectTransform = GetComponent<RectTransform>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (rectTransform != null) 
+		{
+			rectTransform.position = Input.mousePosition;
+			rectTransform.SetAsLastSibling();
+			if (Cursor.visible == true) { Cursor.visible = false; }
+
+			if (Input.GetMouseButtonUp(0) == true)
+			{
+				if (Idle != null) { if (Idle.gameObject.activeSelf == false) { Idle.gameObject.SetActive(true); } }
+				if (Click != null) { if (Click.gameObject.activeSelf == true) { Click.gameObject.SetActive(false); } }
+			}
+			else if (Input.GetMouseButtonDown(0) == true)
+			{
+				if (Idle != null) { if (Idle.gameObject.activeSelf == true) { Idle.gameObject.SetActive(false); } }
+				if (Click != null) { if (Click.gameObject.activeSelf == false) { Click.gameObject.SetActive(true); } }
+			}
+		}
+		else if (rectTransform == null) { Cursor.visible = true; gameObject.SetActive(false); }
+	}
+}
