7eb9eccc2601a904de3e3b5edaa023c615cb3bf0
SuperElectronBrain <qkrdlfwn75@gmail.com>
2023-11-06 20:57:04 +0900

    2023-11-06 change3
    - 메일함 퓨퓨 애니메이션 리소스 적용작업중

--- Assets/5. Scripts/UI/MailBoxUIScript.cs
+++ Assets/5. Scripts/UI/MailBoxUIScript.cs
@@ -2,32 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class MailBoxUIScript : MonoBehaviour
 {
-    public TextMeshProUGUI m_Text;
+	public QuestComponet m_QuestComponet;
+	public TextMeshProUGUI m_Text;
+	[HideInInspector] public UnityEvent<bool> m_OnButtonClick = new UnityEvent<bool>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    
-    //}
-    
-    public void DisplayMail(bool p_Bool, string p_String)
-    {
-        gameObject.SetActive(p_Bool);
-        if (p_Bool == true)
-        {
-			if (m_Text != null)
-            {
-				m_Text.text = p_String;
+	// Start is called before the first frame update
+	void Start()
+	{
+		GameObject t_GO =  UniFunc.GetChildOfName(transform, "AcceptButton");
+		if(t_GO != null)
+		{
+			Button t_AcceptButton = t_GO.GetComponent<Button>();
+			if(t_AcceptButton != null)
+			{
+				t_AcceptButton.onClick.AddListener(() => { OnButtonClick(true); });
 			}
-        }
-    }
+		}
+
+		t_GO = UniFunc.GetChildOfName(transform, "UnacceptButton");
+		if (t_GO != null)
+		{
+			Button t_UnacceptButton = t_GO.GetComponent<Button>();
+			if (t_UnacceptButton != null)
+			{
+				t_UnacceptButton.onClick.AddListener(() => { OnButtonClick(false); });
+			}
+		}
+	}
+
+	void OnButtonClick(bool param)
+	{ m_OnButtonClick.Invoke(param); }
+
+	public void DisplayMail(bool p_Bool, string p_Script)
+	{
+		gameObject.SetActive(p_Bool);
+		if (p_Bool == true)
+		{
+			if (m_Text != null)
+			{
+				m_Text.text = p_Script;
+			}
+		}
+	}
 }
